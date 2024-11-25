8d1388eb648894877697f8cd3a1c6793cf98aeb0
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-11-25 19:59:30 -0300

    Aprendendo Arrays

---
+++ src/Aula07Arrays02.java
@@ -0,0 +1,10 @@
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+
+
+        String[] idades = new String[3];
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
