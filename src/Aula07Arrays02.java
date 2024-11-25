cfa7624db2dbc5eb4b90a907fe15dd6c05243496
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-11-25 20:05:02 -0300

    Arrays usando (For)

--- src/Aula07Arrays02.java
+++ src/Aula07Arrays02.java
@@ -2,9 +2,13 @@ public class Aula07Arrays02 {
     public static void main(String[] args) {
 
 
-        String[] idades = new String[3];
-        System.out.println(idades[0]);
-        System.out.println(idades[1]);
-        System.out.println(idades[2]);
+        String[] nomes = new String[3];
+        nomes[0] = "Goku";
+        nomes[1] = "Kurosaki";
+        nomes[2] = "Luffy";
+
+        for (int i = 0; i < 3; i++){
+            System.out.println(nomes[i]);
+        }
     }
 }
