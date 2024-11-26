8b8e8e24182cb2e32e7b8fef000ddf69ea272847
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-11-25 22:50:59 -0300

    Pequeno laço de repetição com arrays usando for

---
+++ src/Aula08ArraysMultidimensional01.java
@@ -0,0 +1,21 @@
+public class Aula08ArraysMultidimensional01 {
+    public static void main(String[] args) {
+        //1,2,3,4,5 Meses
+        // 31,28,31,30 Dias
+        int[][] dias = new int[2][2];
+        dias[0][0] = 31;
+        dias[0][1] = 28;
+        dias[0][2] = 31;
+
+        dias[1][0] = 31;
+        dias[1][1] = 28;
+        dias[1][2] = 31;
+
+        for (int i = 0; i < dias.length; i++) {
+            for (int j = 0; j < dias[0].length; j++) {
+                System.out.print(dias[i][j]);
+            }
+        }
+
+    }
+}
